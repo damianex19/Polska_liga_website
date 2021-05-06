b5d32b9b1f5a60b8e89666b381b883a8fc0e1a75
damianex19 <damiankarbownik19@vp.pl>
2021-05-06 19:23:28 -0400

    First few steps in web application building

--- Polska_liga.py
+++ Polska_liga.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
+
+clubs = [
+	{  
+		'club': 'Ruch Chorzów',
+		'voivedship': "Silesian",
+		'level': '4',
+		'updatedata': '07.05.2021'
+	},
+	{
+		'club': 'RKS Radomsko',
+		'voivedship': "Łódź",
+		'level': '4',
+		'updatedata': '07.05.2021'
+	}
+]
+
 @app.route('/')
 def home():
-   return 'Witamy na stronie polskich lig krajowych utworzonej z uzyciem microframeworka Flask'
+   return render_template('home.html',clubs=clubs)
+@app.route('/about')
+def about():
+   return render_template('about.html',title='About')
 if __name__ == '__main__':
       app.run(debug=True)
+      
+      
