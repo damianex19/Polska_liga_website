58749dab41ac7ebb2908f7bf151e553e3312e3aa
damianex19 <damiankarbownik19@vp.pl>
2021-05-10 17:40:41 -0400

    Added register and login feature

--- Polska_liga.py
+++ Polska_liga.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for, flash,redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'PUT YOUR KEY HERE'
 
 clubs = [
 	{  
@@ -21,7 +24,21 @@ def home():
    return render_template('home.html',clubs=clubs)
 @app.route('/about')
 def about():
-   return render_template('about.html',title='About')
+   return render_template('about.html',title='About')  
+
+@app.route('/register',methods =["GET","POST"])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!','success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title = 'Register', form = form)	
+@app.route('/login', methods =["GET","POST"])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title = 'Login', form = form)
+	
+
 if __name__ == '__main__':
       app.run(debug=True)
       
